2dc7dca40039e42b1af7a41b52094db772bca5df
suraj-khot-19 <surajkhot19082001@gmail.com>
2024-10-24 12:53:27 +0530

    basic frontend structure

---
+++ ecom_frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Nav from "./components/Nav";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <h1>hi</h1>
+  );
+}
+
+export default App;

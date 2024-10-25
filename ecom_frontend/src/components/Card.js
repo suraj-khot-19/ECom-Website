8d3baff80094a0e9a40a9a7997d99e6f27de75f8
suraj-khot-19 <surajkhot19082001@gmail.com>
2024-10-25 18:47:17 +0530

    fetch image:frontend also done design

--- ecom_frontend/src/components/Card.js
+++ ecom_frontend/src/components/Card.js
@@ -1,10 +1,43 @@
 import { Link } from "react-router-dom";
+import axios from "axios";
+import React, { useState, useEffect } from 'react'
 function Card(props) {
   const { id, name, brand, price } = props.product;
+
+  const [image, setImage] = useState();
+
+  // effect
+  useEffect(() => {
+    fetchImage();
+  }, [id])
+
+
+  // function to fetch product image
+  async function fetchImage() {
+    let url = ` http://localhost:8080/api/product/${id}/image`;
+    try {
+      // fire to get
+      const response = await axios.get(
+        url,
+        {
+          // response type is blob
+          responseType: 'blob'
+        }
+      );
+      console.log("response:", response)
+      const imgurl = URL.createObjectURL((await response).data);
+      setImage(imgurl);
+    } catch (error) {
+      console.log(error);
+      setImage('');
+    }
+
+  }
   return (
     <>
       <Link to={`product/${id}`} style={{ textDecoration: 'none' }}>
         <div className="card m-2" style={{ color: 'white', backgroundColor: 'rgb(60,60,60,60)' }}>
+          <img className="card-img-top" alt="product img" src={image} />
           <div className="card-body">
             <h5 className="card-title">{name}</h5>
             <p className="card-text">{brand}</p>
--- ecom_frontend/src/components/Product.js
+++ ecom_frontend/src/components/Product.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-import image from './notfound.png';
+import notFoundImage from './notfound.png';
+import axios from 'axios';
 
 export default function Product() {
     const [singleProduct, setSingleProduct] = useState([]);
     const [notFound, setNotFound] = useState(false)
+
+    // image
+    const [image, setImage] = useState(null);
 
     //get id from parameter which is comming as id
     const { id } = useParams();
@@ -25,9 +29,31 @@ export default function Product() {
         }
     }
 
+    //fetching an image
+    async function fetchImageOfProduct() {
+        try {
+            const url = `http://localhost:8080/api/product/${id}/image`;
+            const response = await axios(url,
+                {
+                    //type of blob
+                    responseType: 'blob',
+                }
+            );
+
+            //format url
+            const imgUrl = URL.createObjectURL(await (response.data));
+
+            setImage(imgUrl);
+        } catch (error) {
+            console.log(error);
+            setImage(notFoundImage);
+        }
+    }
+
     useEffect(() => {
         if (id) {
             fetchingSingleProduct();
+            fetchImageOfProduct();
         }
         // eslint-disable-next-line
     }, [id])
@@ -39,44 +65,59 @@ export default function Product() {
             {
                 //if no product
                 notFound ? <div className='container m-auto text-center'>
-                    <img src={image} alt='Product not found....' style={{ height: '80vh' }} /></div> :
+                    <img src={notFoundImage} alt='Product not found....' style={{ height: '80vh' }} /></div> :
 
                     //if product
-                    <div className="container-md">
-                        <div className='card pt-4 d-flex justify-content-center align-items-center' style={{ color: 'white', backgroundColor: 'transparent' }}>
-                            <div className="card-body">
+                    <div className="container m-auto h-100">
+                        <div className='d-flex justify-content-center align-items-center h-100'>
 
-                                <p><i style={{ color: 'cyan', fontWeight: 'bolder' }}>{category?.toUpperCase()}</i></p>
+                            {/* image */}
+                            <div style={{ width: '50%' }}>
+                                <img src={image} alt='not found ...' style={{
+                                    width: '100%', height: 'auto', maxHeight: '100%', objectFit: 'contain', maxWidth: '80%'
+                                }} />
+                            </div>
 
-                                <h2 className="card-title">{name}</h2>
+                            {/* disc */}
+                            <div className='card pt-4 d-flex justify-content-center align-items-center' style={{ color: 'white', backgroundColor: 'transparent', width: '50%' }}>
+                                <div className="card-body">
 
-                                <h6><i style={{ fontWeight: 'bolder' }}>{brand}</i></h6>
+                                    <p><i style={{ color: 'cyan', fontWeight: 'bolder' }}>{category?.toUpperCase()}</i></p>
 
-                                <hr />
+                                    <h2 className="card-title">{name}</h2>
 
-                                <p className="card-text">{disc}</p>
+                                    <h6><i style={{ fontWeight: 'bolder' }}>{brand}</i></h6>
 
-                                <p className="card-text" style={{ fontSize: '1.3rem' }}>{price} Rs</p>
+                                    <hr />
 
-                                <div className="btn btn-outline-light mb-2">
-                                    {available ? 'Add To Cart' : 'Out Of Stock'}
-                                </div>
+                                    <p className="card-text">{disc}</p>
 
-                                <p className="card-text">Stock Available: {quantity}</p>
+                                    <p className="card-text" style={{ fontSize: '1.3rem' }}>{price} Rs</p>
 
-                                Product listed on:
-                                <p className="card-text"><i>{release_date}</i></p>
-
-                                <div className="d-flex">
-                                    <button to="/" className="btn btn-outline-light me-2">
-                                        Update
+                                    <button disabled={!available} className="btn btn-outline-light mb-2">
+                                        {available ? 'Add To Cart' : 'Out Of Stock'}
                                     </button>
-                                    <button to="/" className="btn btn-outline-light">
-                                        Delete
-                                    </button>
+
+                                    <p className="card-text">Stock Available: {quantity}</p>
+
+                                    Product listed on:
+                                    <p className="card-text"><i>{release_date}</i></p>
+
+                                    <div className="d-flex">
+                                        <button to="/" className="btn btn-outline-light me-2">
+                                            Update
+                                        </button>
+                                        <button to="/" className="btn btn-outline-light">
+                                            Delete
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
+
+
                         </div>
+
+
                     </div>
             }
         </>

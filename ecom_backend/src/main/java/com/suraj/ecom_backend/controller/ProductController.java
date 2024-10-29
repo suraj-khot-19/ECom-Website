dcda62aef6546a41369811a74529b8cca9140009
suraj-khot-19 <surajkhot19082001@gmail.com>
2024-10-29 18:01:31 +0530

    Re-add mvnw as executable

--- ecom_backend/src/main/java/com/suraj/ecom_backend/controller/ProductController.java
+++ ecom_backend/src/main/java/com/suraj/ecom_backend/controller/ProductController.java
@@ -12,6 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
+//controller
 @RestController
 @RequestMapping("/api/v2")
 public class ProductController {
